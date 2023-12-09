3c4d394906cd3fe8f6e632f8b7bd71c29df1ac87
StefanVDWeide <stefan.vander.weide@hotmail.com>
2023-12-10 00:18:27 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,450 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <header class="px-4 lg:px-6 h-14 flex items-center"><a class="flex items-center justify-center" href="#"><svg
+          xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+          stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6">
+          <circle cx="12" cy="12" r="10"></circle>
+          <polyline points="12 6 12 12 16 14"></polyline>
+        </svg><span class="sr-only">Time Display</span></a>
+    </header>
+    <main class="flex-1 py-12 md:py-24 lg:py-32 xl:py-48">
+      <div class="container px-4 md:px-6">
+        <div class="flex flex-col items-center space-y-4 gap-4 text-center">
+          <div class="space-y-2">
+            <h1 class="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+              Is it 4 PM yet? {{ isTimeBetween4PMandMidnight(currentTime) ? "Yes!" : "No :(" }}
+            </h1>
+            <p class="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+              Find out where in the world it's drinking time!
+            </p>
+          </div>
+          <div
+            class="inline-flex items-center rounded-full border px-2.5 py-0.5 font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80 text-2xl">
+            Current Time: {{ currentTime }}
+          </div>
+          <p class="text-gray-500 md:text-lg dark:text-gray-400">Timezone: {{ currentTimezone }}</p>
+          <h2 class="mt-8 text-2xl font-bold tracking-tighter sm:text-3xl md:text-4xl lg:text-5xl/none">
+            Cities where it's after 4 PM:
+          </h2>
+          <div class="mt-8 w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            <div v-for="city in citiesTimeAfterFourPM" class="border text-card-foreground bg-white shadow-sm rounded-lg">
+              <div class="flex-col space-y-1.5 p-6 flex items-center justify-between">
+                <h3 class="text-lg font-semibold">{{ city.name.replace('_', ' ') }}</h3>
+                <h4 class="">{{ city.time }}</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    <footer class="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+      <p class="text-xs text-gray-500 dark:text-gray-400">
+        © All rights reserved.
+      </p>
+
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+type City = {
+  name: string;
+  timezone: string;
+  time: string;
+};
+
+const currentTime = ref('');
+const currentTimezone = ref('');
+const citiesTimeAfterFourPM = ref<City[]>([]);
+
+onMounted(() => {
+  getCurrentTimeAndTimezone();
+  getCitiesInTimezone();
+});
+
+const getCurrentTimeAndTimezone = (): void => {
+  const now = new Date();
+
+  currentTime.value = now.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+  currentTimezone.value = Intl.DateTimeFormat().resolvedOptions().timeZone;
+};
+
+const isTimeBetween4PMandMidnight = (strTime: string): boolean => {
+  const timePart = strTime.split(',')[1]?.trim();
+
+  if (!timePart) return false;
+
+  const time = new Date('1970-01-01 ' + timePart);
+  const hours = time.getHours();
+
+  return hours >= 16 && hours < 24;
+};
+
+const getCitiesInTimezone = () => {
+  const aryIanaTimeZones = [
+    'Europe/Andorra',
+    'Asia/Dubai',
+    'Asia/Kabul',
+    'Europe/Tirane',
+    'Asia/Yerevan',
+    'Antarctica/Casey',
+    'Antarctica/Davis',
+    'Antarctica/DumontDUrville',
+    'Antarctica/Mawson',
+    'Antarctica/Palmer',
+    'Antarctica/Rothera',
+    'Antarctica/Syowa',
+    'Antarctica/Troll',
+    'Antarctica/Vostok',
+    'America/Argentina/Buenos_Aires',
+    'America/Argentina/Cordoba',
+    'America/Argentina/Salta',
+    'America/Argentina/Jujuy',
+    'America/Argentina/Tucuman',
+    'America/Argentina/Catamarca',
+    'America/Argentina/La_Rioja',
+    'America/Argentina/San_Juan',
+    'America/Argentina/Mendoza',
+    'America/Argentina/San_Luis',
+    'America/Argentina/Rio_Gallegos',
+    'America/Argentina/Ushuaia',
+    'Pacific/Pago_Pago',
+    'Europe/Vienna',
+    'Australia/Lord_Howe',
+    'Antarctica/Macquarie',
+    'Australia/Hobart',
+    'Australia/Currie',
+    'Australia/Melbourne',
+    'Australia/Sydney',
+    'Australia/Broken_Hill',
+    'Australia/Brisbane',
+    'Australia/Lindeman',
+    'Australia/Adelaide',
+    'Australia/Darwin',
+    'Australia/Perth',
+    'Australia/Eucla',
+    'Asia/Baku',
+    'America/Barbados',
+    'Asia/Dhaka',
+    'Europe/Brussels',
+    'Europe/Sofia',
+    'Atlantic/Bermuda',
+    'Asia/Brunei',
+    'America/La_Paz',
+    'America/Noronha',
+    'America/Belem',
+    'America/Fortaleza',
+    'America/Recife',
+    'America/Araguaina',
+    'America/Maceio',
+    'America/Bahia',
+    'America/Sao_Paulo',
+    'America/Campo_Grande',
+    'America/Cuiaba',
+    'America/Santarem',
+    'America/Porto_Velho',
+    'America/Boa_Vista',
+    'America/Manaus',
+    'America/Eirunepe',
+    'America/Rio_Branco',
+    'America/Nassau',
+    'Asia/Thimphu',
+    'Europe/Minsk',
+    'America/Belize',
+    'America/St_Johns',
+    'America/Halifax',
+    'America/Glace_Bay',
+    'America/Moncton',
+    'America/Goose_Bay',
+    'America/Blanc-Sablon',
+    'America/Toronto',
+    'America/Nipigon',
+    'America/Thunder_Bay',
+    'America/Iqaluit',
+    'America/Pangnirtung',
+    'America/Atikokan',
+    'America/Winnipeg',
+    'America/Rainy_River',
+    'America/Resolute',
+    'America/Rankin_Inlet',
+    'America/Regina',
+    'America/Swift_Current',
+    'America/Edmonton',
+    'America/Cambridge_Bay',
+    'America/Yellowknife',
+    'America/Inuvik',
+    'America/Creston',
+    'America/Dawson_Creek',
+    'America/Fort_Nelson',
+    'America/Vancouver',
+    'America/Whitehorse',
+    'America/Dawson',
+    'Indian/Cocos',
+    'Europe/Zurich',
+    'Africa/Abidjan',
+    'Pacific/Rarotonga',
+    'America/Santiago',
+    'America/Punta_Arenas',
+    'Pacific/Easter',
+    'Asia/Shanghai',
+    'Asia/Urumqi',
+    'America/Bogota',
+    'America/Costa_Rica',
+    'America/Havana',
+    'Atlantic/Cape_Verde',
+    'America/Curacao',
+    'Indian/Christmas',
+    'Asia/Nicosia',
+    'Asia/Famagusta',
+    'Europe/Prague',
+    'Europe/Berlin',
+    'Europe/Copenhagen',
+    'America/Santo_Domingo',
+    'Africa/Algiers',
+    'America/Guayaquil',
+    'Pacific/Galapagos',
+    'Europe/Tallinn',
+    'Africa/Cairo',
+    'Africa/El_Aaiun',
+    'Europe/Madrid',
+    'Africa/Ceuta',
+    'Atlantic/Canary',
+    'Europe/Helsinki',
+    'Pacific/Fiji',
+    'Atlantic/Stanley',
+    'Pacific/Chuuk',
+    'Pacific/Pohnpei',
+    'Pacific/Kosrae',
+    'Atlantic/Faroe',
+    'Europe/Paris',
+    'Europe/London',
+    'Asia/Tbilisi',
+    'America/Cayenne',
+    'Africa/Accra',
+    'Europe/Gibraltar',
+    'America/Godthab',
+    'America/Danmarkshavn',
+    'America/Scoresbysund',
+    'America/Thule',
+    'Europe/Athens',
+    'Atlantic/South_Georgia',
+    'America/Guatemala',
+    'Pacific/Guam',
+    'Africa/Bissau',
+    'America/Guyana',
+    'Asia/Hong_Kong',
+    'America/Tegucigalpa',
+    'America/Port-au-Prince',
+    'Europe/Budapest',
+    'Asia/Jakarta',
+    'Asia/Pontianak',
+    'Asia/Makassar',
+    'Asia/Jayapura',
+    'Europe/Dublin',
+    'Asia/Jerusalem',
+    'Asia/Kolkata',
+    'Indian/Chagos',
+    'Asia/Baghdad',
+    'Asia/Tehran',
+    'Atlantic/Reykjavik',
+    'Europe/Rome',
+    'America/Jamaica',
+    'Asia/Amman',
+    'Asia/Tokyo',
+    'Africa/Nairobi',
+    'Asia/Bishkek',
+    'Pacific/Tarawa',
+    'Pacific/Enderbury',
+    'Pacific/Kiritimati',
+    'Asia/Pyongyang',
+    'Asia/Seoul',
+    'Asia/Almaty',
+    'Asia/Qyzylorda',
+    'Asia/Qostanay',
+    'Asia/Aqtobe',
+    'Asia/Aqtau',
+    'Asia/Atyrau',
+    'Asia/Oral',
+    'Asia/Beirut',
+    'Asia/Colombo',
+    'Africa/Monrovia',
+    'Europe/Vilnius',
+    'Europe/Luxembourg',
+    'Europe/Riga',
+    'Africa/Tripoli',
+    'Africa/Casablanca',
+    'Europe/Monaco',
+    'Europe/Chisinau',
+    'Pacific/Majuro',
+    'Pacific/Kwajalein',
+    'Asia/Yangon',
+    'Asia/Ulaanbaatar',
+    'Asia/Hovd',
+    'Asia/Choibalsan',
+    'Asia/Macau',
+    'America/Martinique',
+    'Europe/Malta',
+    'Indian/Mauritius',
+    'Indian/Maldives',
+    'America/Mexico_City',
+    'America/Cancun',
+    'America/Merida',
+    'America/Monterrey',
+    'America/Matamoros',
+    'America/Mazatlan',
+    'America/Chihuahua',
+    'America/Ojinaga',
+    'America/Hermosillo',
+    'America/Tijuana',
+    'America/Bahia_Banderas',
+    'Asia/Kuala_Lumpur',
+    'Asia/Kuching',
+    'Africa/Maputo',
+    'Africa/Windhoek',
+    'Pacific/Noumea',
+    'Pacific/Norfolk',
+    'Africa/Lagos',
+    'America/Managua',
+    'Europe/Amsterdam',
+    'Europe/Oslo',
+    'Asia/Kathmandu',
+    'Pacific/Nauru',
+    'Pacific/Niue',
+    'Pacific/Auckland',
+    'Pacific/Chatham',
+    'America/Panama',
+    'America/Lima',
+    'Pacific/Tahiti',
+    'Pacific/Marquesas',
+    'Pacific/Gambier',
+    'Pacific/Port_Moresby',
+    'Pacific/Bougainville',
+    'Asia/Manila',
+    'Asia/Karachi',
+    'Europe/Warsaw',
+    'America/Miquelon',
+    'Pacific/Pitcairn',
+    'America/Puerto_Rico',
+    'Asia/Gaza',
+    'Asia/Hebron',
+    'Europe/Lisbon',
+    'Atlantic/Madeira',
+    'Atlantic/Azores',
+    'Pacific/Palau',
+    'America/Asuncion',
+    'Asia/Qatar',
+    'Indian/Reunion',
+    'Europe/Bucharest',
+    'Europe/Belgrade',
+    'Europe/Kaliningrad',
+    'Europe/Moscow',
+    'Europe/Simferopol',
+    'Europe/Kirov',
+    'Europe/Astrakhan',
+    'Europe/Volgograd',
+    'Europe/Saratov',
+    'Europe/Ulyanovsk',
+    'Europe/Samara',
+    'Asia/Yekaterinburg',
+    'Asia/Omsk',
+    'Asia/Novosibirsk',
+    'Asia/Barnaul',
+    'Asia/Tomsk',
+    'Asia/Novokuznetsk',
+    'Asia/Krasnoyarsk',
+    'Asia/Irkutsk',
+    'Asia/Chita',
+    'Asia/Yakutsk',
+    'Asia/Khandyga',
+    'Asia/Vladivostok',
+    'Asia/Ust-Nera',
+    'Asia/Magadan',
+    'Asia/Sakhalin',
+    'Asia/Srednekolymsk',
+    'Asia/Kamchatka',
+    'Asia/Anadyr',
+    'Asia/Riyadh',
+    'Pacific/Guadalcanal',
+    'Indian/Mahe',
+    'Africa/Khartoum',
+    'Europe/Stockholm',
+    'Asia/Singapore',
+    'America/Paramaribo',
+    'Africa/Juba',
+    'Africa/Sao_Tome',
+    'America/El_Salvador',
+    'Asia/Damascus',
+    'America/Grand_Turk',
+    'Africa/Ndjamena',
+    'Indian/Kerguelen',
+    'Asia/Bangkok',
+    'Asia/Dushanbe',
+    'Pacific/Fakaofo',
+    'Asia/Dili',
+    'Asia/Ashgabat',
+    'Africa/Tunis',
+    'Pacific/Tongatapu',
+    'Europe/Istanbul',
+    'America/Port_of_Spain',
+    'Pacific/Funafuti',
+    'Asia/Taipei',
+    'Europe/Kiev',
+    'Europe/Uzhgorod',
+    'Europe/Zaporozhye',
+    'Pacific/Wake',
+    'America/New_York',
+    'America/Detroit',
+    'America/Kentucky/Louisville',
+    'America/Kentucky/Monticello',
+    'America/Indiana/Indianapolis',
+    'America/Indiana/Vincennes',
+    'America/Indiana/Winamac',
+    'America/Indiana/Marengo',
+    'America/Indiana/Petersburg',
+    'America/Indiana/Vevay',
+    'America/Chicago',
+    'America/Indiana/Tell_City',
+    'America/Indiana/Knox',
+    'America/Menominee',
+    'America/North_Dakota/Center',
+    'America/North_Dakota/New_Salem',
+    'America/North_Dakota/Beulah',
+    'America/Denver',
+    'America/Boise',
+    'America/Phoenix',
+    'America/Los_Angeles',
+    'America/Anchorage',
+    'America/Juneau',
+    'America/Sitka',
+    'America/Metlakatla',
+    'America/Yakutat',
+    'America/Nome',
+    'America/Adak',
+    'Pacific/Honolulu',
+    'America/Montevideo',
+    'Asia/Samarkand',
+    'Asia/Tashkent',
+    'America/Caracas',
+    'Asia/Ho_Chi_Minh',
+    'Pacific/Efate',
+    'Pacific/Wallis',
+    'Pacific/Apia',
+    'Africa/Johannesburg'
+  ];
+
+  aryIanaTimeZones.forEach((timeZone: string) => {
+    let strTime = new Date().toLocaleString("en-US", { timeZone: `${timeZone}` });
+
+    if (isTimeBetween4PMandMidnight(strTime)) {
+      const city: City = {
+        name: timeZone.split("/").pop() ?? "Unknown",
+        timezone: timeZone,
+        time: strTime.split(",")[1].trim()
+      };
+      citiesTimeAfterFourPM.value.push(city);
+    }
+  });
+};
+
+</script>
+
